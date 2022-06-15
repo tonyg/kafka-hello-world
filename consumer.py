9a319594c76264c021e3c27b95190de7f49f0841
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2022-06-15 11:56:39 +0200

    More experiments

--- consumer.py
+++ consumer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import time
 from kafka import KafkaConsumer
 
 # import logging
@@ -13,6 +14,26 @@ consumer = KafkaConsumer('hello-world')
 # for m in consumer:
 #     print(m)
 
+# while True:
+#     print('Polling')
+#     print(consumer.poll(timeout_ms=5000))
+
+# while True:
+#     print('Polling')
+#     print(consumer.poll(timeout_ms=0))
+#     print('Sleeping')
+#     time.sleep(5)
+
+# while True:
+#     print('Polling')
+#     print(consumer.poll(timeout_ms=1))
+#     print('Sleeping')
+#     time.sleep(5)
+
 while True:
     print('Polling')
-    print(consumer.poll(timeout_ms=5000))
+    ms=0  # 1 and 5 both seem to work just fine, 0 I'm not 100% on
+    print(consumer.poll(timeout_ms=ms))
+    print(consumer.poll(timeout_ms=ms))
+    print('Sleeping')
+    time.sleep(5 - (2 * ms / 1000.0))
